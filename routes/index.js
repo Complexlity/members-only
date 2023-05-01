f5fc5b6f241c5602cd958b6fc23ceb5bc354d9e5
Complexlity <105590967+Complexlity@users.noreply.github.com>
2023-05-01 11:09:24 +0100

    feat: create routes and views

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@ let express = require("express");
 let router = express.Router();
 
 /* GET home page. */
+router.get("/join", (req, res, next) => {
+  res.render("join", { title: "Join The Club" });
+});
+router.get("/login", (req, res, next) => {
+  res.render("login", { title: "Log In To Your Account" });
+});
+router.get("/signup", (req, res, next) => {
+  res.render("signup", { title: "Create A New Account" });
+});
+
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Complex Club" });
 });
 
 module.exports = router;
